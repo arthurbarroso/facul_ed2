acfbf1616d958d08ff8661a495efa9d99dcdb931
arthurbarroso <arthurdsb_@hotmail.com>
2024-09-09 07:51:18 -0300

    x

--- src/avl/avl.h
+++ src/avl/avl.h
@@ -21,8 +21,6 @@ void cleanup(Node *node);
 void print_tree(Node *head);
 
 Node *new_node(int val);
-Node *search(Node *head, int val);
-Node *find_parent(Node *current, Node *parent, int val);
 Node *insert_node(Node *head, int val);
 Node *remove_node(Node *head, int val);
 int get_node_balance(Node *node);
@@ -30,8 +28,6 @@ int get_node_balance(Node *node);
 Node *rotate_right(Node *root);
 Node *rotate_left(Node *node);
 
-bool is_leaf(Node *head);
-int find_min_val_in_right_subtree(Node *root);
 int get_height(Node *node);
 void pretty_print(Node *node, int indent);
 
